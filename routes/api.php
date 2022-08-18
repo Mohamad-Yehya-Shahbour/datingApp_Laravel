c5a25cfa11de234c64cab982e25de1dd013e3433
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-18 23:55:22 +0300

    add refresh route

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,5 @@ Route::group([
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
 });
