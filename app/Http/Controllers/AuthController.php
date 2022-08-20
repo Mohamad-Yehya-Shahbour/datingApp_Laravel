21dea53ca88f70d9519b53c9cee1f30b286c7157
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-20 20:31:44 +0300

    add contruct for auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,4 +10,12 @@ use Validator;
 
 class AuthController extends Controller
 {
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
 }
