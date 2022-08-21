5151e7e23b6b15f31495c2b0267ebf84a8dcc220
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-22 00:16:26 +0300

    add admins controller

---
+++ app/Http/Controllers/AdminsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\FavoriteRequest;
+use App\Models\Chat;
+use App\Models\MatchUser;
+use App\Models\Notification;
+use App\Models\Picture;
+use Illuminate\Support\Facades\DB;
+use Validator;
+use Auth;
+
+class AdminsController extends Controller
+{
+}
