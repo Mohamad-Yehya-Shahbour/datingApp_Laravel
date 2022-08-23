1869713b42129eefb154379aa73de277570151f6
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-23 20:16:51 +0300

    add search function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -73,4 +73,29 @@ class UsersController extends Controller
             'user' => $user
         ], 201);
     }
+
+    public function search(Request $request){
+
+        $userGender = auth()->user()->gender; // get user gender
+        $userId = auth()->user()->id; // get user id
+
+        $favoriteRequestId = FavoriteRequest::where('favorite_requests.from', '=', $userId)
+        ->pluck('to')->all(); // getting ids of people who favorite user
+
+        // get people who are matched with user
+        $matchedForUserId_1 = MatchUser::where('matches.user_1', '=', $userId)->pluck('user_2')->all();
+        $matchedForUserId_2 = MatchUser::where('matches.user_2', '=', $userId)->pluck('user_1')->all();
+        $matchedUsers = array_merge($matchedForUserId_1,$matchedForUserId_2);
+
+        // selecting users who are not matched or already been favorited with opposite gender & not admin based on search-input 
+        $users = User::whereNotIn('id', $favoriteRequestId)
+        ->whereNotIn('id', $matchedUsers)
+        ->select("*")
+        ->where('gender', '!=', $userGender)
+        ->where('userType', '!=', '1')
+        ->where('name', 'Like', '%' . $request -> key . '%')
+        ->get();
+
+        return json_encode($users, JSON_PRETTY_PRINT);
+    }
 }
