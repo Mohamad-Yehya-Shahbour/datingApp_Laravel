b05d964f327e3f2429acba289fa12265497b29e2
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-23 20:14:58 +0300

    add users controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\FavoriteRequest;
+use App\Models\Chat;
+use App\Models\MatchUser;
+use App\Models\Notification;
+use App\Models\Picture;
+use Illuminate\Support\Facades\DB;
+use Validator;
+
+
+
+class UsersController extends Controller
+{
+}
